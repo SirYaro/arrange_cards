f5d36a12194aa88cc59dcc2f85a1c8a7b70e2446
SirYaro <jaroslaw@czarniak.org>
2016-10-01 17:08:01 +0200

    fixed switch

--- sample_env2/sample_generate.sh
+++ sample_env2/sample_generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../arrange.sh -i arrange3.txt -o filename.pdf -f zetony_setup.png -c 144 -d zetony_CS.sh
+../arrange.sh -i arrange3.txt -o filename.pdf -f zetony_setup.png -c 144 -s zetony_CS.sh
