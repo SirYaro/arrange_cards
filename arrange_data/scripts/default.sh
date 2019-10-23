2710a8f0e99b061cf397f459f31f08743e8a36be
SirYaro <jaroslaw@czarniak.org>
2019-10-23 11:20:18 +0200

    .

--- arrange_data/scripts/default.sh
+++ arrange_data/scripts/default.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 
 page=$(echo $1|rev|cut -f1 -d";"|rev)
@@ -20,7 +20,7 @@ for r in $(seq 1 ${rows}); do
 	if [ "${plik}" != "" ]; then 
 	    echo -n "Processing file ${plik}..."
 	    cp -f ${plik} /tmp/plik
-	    if [ $RESIZE -gt 0 ]; then
+	    if [ ${RESIZE} -gt 0 ]; then
 		if [ $RESIZE -eq 1 ]; then
 		    convert -resize ${WIDTH}x${HEIGHT} /tmp/plik /tmp/plik
 		elif [ $RESIZE -eq 2 ]; then
@@ -47,10 +47,10 @@ for r in $(seq 1 ${rows}); do
 	
 	#MARKERS
 	if [ $MARKER_ENABLED -eq 1 ]; then
-	    composite -verbose  -geometry +$((START_X + X - 25 + MARKER_X))+$((START_Y + Y - 25 + MARKER_Y)) ${DATA_DIR}/imgs/marker.png /tmp/montage.png /tmp/montage.png > /dev/null 2>&1		#TOP LEFT CORNER
-	    composite -verbose  -geometry +$((START_X + X - 25 - MARKER_X + SZER))+$((START_Y + Y - 25 + MARKER_Y)) ${DATA_DIR}/imgs/marker.png /tmp/montage.png /tmp/montage.png > /dev/null 2>&1	#TOP RIGHT CORNER
-	    composite -verbose  -geometry +$((START_X + X - 25 + MARKER_X))+$((START_Y + Y - 25 - MARKER_Y + WYS)) ${DATA_DIR}/imgs/marker.png /tmp/montage.png /tmp/montage.png > /dev/null 2>&1	#BOTTOM LEFT CORNER
-	    composite -verbose  -geometry +$((START_X + X - 25 - MARKER_X + SZER))+$((START_Y + Y - 25 - MARKER_Y + WYS)) ${DATA_DIR}/imgs/marker.png /tmp/montage.png /tmp/montage.png > /dev/null 2>&1	#BOTTOM RIGHT CORNER
+	    composite -verbose  -geometry +$((START_X + X - 25 + MARKER_X))+$((START_Y + Y - 25 + MARKER_Y)) ${DATA_DIR}/imgs/${MARKER} /tmp/montage.png /tmp/montage.png > /dev/null 2>&1		#TOP LEFT CORNER
+	    composite -verbose  -geometry +$((START_X + X - 25 - MARKER_X + SZER))+$((START_Y + Y - 25 + MARKER_Y)) ${DATA_DIR}/imgs/${MARKER} /tmp/montage.png /tmp/montage.png > /dev/null 2>&1	#TOP RIGHT CORNER
+	    composite -verbose  -geometry +$((START_X + X - 25 + MARKER_X))+$((START_Y + Y - 25 - MARKER_Y + WYS)) ${DATA_DIR}/imgs/${MARKER} /tmp/montage.png /tmp/montage.png > /dev/null 2>&1	#BOTTOM LEFT CORNER
+	    composite -verbose  -geometry +$((START_X + X - 25 - MARKER_X + SZER))+$((START_Y + Y - 25 - MARKER_Y + WYS)) ${DATA_DIR}/imgs/${MARKER} /tmp/montage.png /tmp/montage.png > /dev/null 2>&1	#BOTTOM RIGHT CORNER
 	fi
 	
 	X=$((X + SZER + GAP))
@@ -61,23 +61,24 @@ for r in $(seq 1 ${rows}); do
 	if [ $file_no -gt $files_number ]; then	# finish if img count on page is < than max
 	    break 2
 	fi
-	tt=$((($(date +%s%N) - ${ts})/1000000))
-	ttt=$((tt + ttt))
-	att=$((ttt / FILE_NUMBER))
-	fl=$((all_files_num - FILE_NUMBER))
-	tl=$((att * fl / 6000))
+#	tt=$((($(date +%s%N) - ${ts})/1000000))
+#	ttt=$((tt + ttt))
+#	att=$((ttt / FILE_NUMBER))
+#	fl=$((all_files_num - FILE_NUMBER))
+#	tl=$((att * fl / 6000))
 #	echo -en "\nTime taken: ${tt}ms\n"
 #	echo -en "Total time taken: ${ttt}ms\n"
-	echo -en ", time left: <${tl} min\n"
+#	echo -en ", time left: <${tl} min\n"
 #	echo -en "\t(average time taken per file: ${att}ms)\n"
+	echo -en "\n"
     done
     Y=$((Y + WYS + GAP));
     X=0				# reset x axis
 done
 
 echo -en "\nGenerating pdf page.\n"
-composite -verbose -geometry +0+0 ${DATA_DIR}/overlays/${FRAME} /tmp/montage.png page_${page}_${TIMESTAMP}.png > /dev/null 2>&1		# ADD OVERLAY
+composite -verbose -geometry +0+0 ${DATA_DIR}/overlays/${FRAME} +profile "*" /tmp/montage.png page_${page}_${TIMESTAMP}.png > /dev/null 2>&1		# ADD OVERLAY
 if [ ${REVERSE} -eq 1 ]; then
-	convert -flop page_${page}_${TIMESTAMP}.png page_${page}_${TIMESTAMP}.png	#REWERS
+	convert -flop page_${page}_${TIMESTAMP}.png +profile "*" page_${page}_${TIMESTAMP}.png	#REWERS
 fi
 rm -f /tmp/montage.png
