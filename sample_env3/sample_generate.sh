ab20203da84990cfd3cc67cc6338b0947d98d70a
Sir_Yaro <sir_yaro@.pl>
2021-12-30 15:25:54 +0100

    new sample

---
+++ sample_env3/sample_generate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../arrange.sh -i hexY.txt -o filename.pdf -f empty.png -s hexy_38mm.sh -c 6 -r 5 -x 35 -y 51 --mask hex_37mm_v3.png --landscape -b HEX37MM --rmup 143 --rmdown 143 --rmleft 210 --rmright 210 -p 50 --crop --gapx -82 --gapy -150
