027a0fa13e41c112f33a994dc5dccb11235bb22d
Sir_Yaro <sir_yaro@.pl>
2021-12-31 16:32:46 +0100

    new sample with default module

---
+++ sample_env5/_sample_generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NAME="filename"
+
+rm -f ${NAME}*.pdf
+
+arrange.sh -i zetony.txt -o ${NAME}.pdf -f empty.png  -c 12 -r 17 -x 100 -y 100 --gapx 0 --gapy 0 -b fffce9 -m cross_black.png --my 0 --mx 0
+
