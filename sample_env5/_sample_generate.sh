1beef1f69421279f9645701e2a0fcd37e95b6bbf
Sir_Yaro <jaroslaw@czarniak.org>
2022-01-01 14:31:34 +0100

    Update _sample_generate.sh

--- sample_env5/_sample_generate.sh
+++ sample_env5/_sample_generate.sh
@@ -4,5 +4,5 @@ NAME="filename"
 
 rm -f ${NAME}*.pdf
 
-arrange.sh -i zetony.txt -o ${NAME}.pdf -f empty.png  -c 12 -r 17 -x 100 -y 100 --gapx 0 --gapy 0 -b fffce9 -m cross_black.png --my 0 --mx 0
+../arrange.sh -i zetony.txt -o ${NAME}.pdf -f empty.png  -c 12 -r 17 -x 100 -y 100 --gapx 0 --gapy 0 -b fffce9 -m cross_black.png --my 0 --mx 0
 
